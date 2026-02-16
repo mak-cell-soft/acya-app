02797b919aa1c3886903e3072b5190a0ca2b1167
Amine KLABI <medamine.klabi@outlook.com>
2026-02-16 09:22:26 +0100

    fix: date to compare te get payments

--- wood-app.api/src/ms.webapp.api.acya.infrastructure/Repositories/PaymentRepository.cs
+++ wood-app.api/src/ms.webapp.api.acya.infrastructure/Repositories/PaymentRepository.cs
@@ -112,9 +112,6 @@ namespace ms.webapp.api.acya.infrastructure.Repositories
         }
         public async Task<IEnumerable<DashboardPaymentDto>> GetDashboardPaymentsAsync(DateTime date, int salesSiteId)
         {
-            var startDate = date.Date;
-            var endDate = startDate.AddDays(1);
-
             return await context.Payments
                 .AsNoTracking()
                 .Include(p => p.Customer)
@@ -125,7 +122,7 @@ namespace ms.webapp.api.acya.infrastructure.Repositories
                     .ThenInclude(d => d!.ParentDocuments)
                         .ThenInclude(pd => pd!.ParentDocument)
                 .Where(p => !p.IsDeleted)
-                .Where(p => p.PaymentDate >= startDate && p.PaymentDate < endDate)
+                .Where(p => p.PaymentDate.HasValue && p.PaymentDate.Value.Date == date.Date)
                 .Where(p => p.Document!.SalesSiteId == salesSiteId)
                 .OrderByDescending(p => p.CreatedAt)
                 .Select(p => new DashboardPaymentDto
