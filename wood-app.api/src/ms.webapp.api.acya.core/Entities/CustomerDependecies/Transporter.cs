da302c6fb566807f56d8871c79608a79d4fdccaa
Amine KLABI <medamine.klabi@outlook.com>
2026-03-23 10:25:00 +0100

    fix. Transporter duplication

--- wood-app.api/src/ms.webapp.api.acya.core/Entities/CustomerDependecies/Transporter.cs
+++ wood-app.api/src/ms.webapp.api.acya.core/Entities/CustomerDependecies/Transporter.cs
@@ -9,8 +9,24 @@ namespace ms.webapp.api.acya.core.Entities.CustomerDependecies
     public string? LastName { get; set; }
     public string FullName => Helpers.CapitalizeFirstLetter(FirstName!) + " " + LastName!.ToUpper();
 
-    
     public int? CarId { get; set; }
     public Vehicle? car { get; set; }
+
+    public Transporter()
+    {
+    }
+
+    public Transporter(TransporterDto dto)
+    {
+      UpdateFromDto(dto);
+    }
+
+    public void UpdateFromDto(TransporterDto dto)
+    {
+      Id = dto.id;
+      FirstName = Helpers.CapitalizeFirstLetter(dto.firstname); ;
+      LastName = dto.lastname!.ToUpper();
+      FullName = Helpers.CapitalizeFirstLetter(FirstName!) + " " + LastName!.ToUpper();
+    }
   }
 }
